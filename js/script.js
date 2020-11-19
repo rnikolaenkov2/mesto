fc9506794f13e7e06aaf72c79e6b499b2b691034
Roman Nikolaenkov <rnikolaenkov@gmail.com>
2020-11-19 09:35:28 +0300

    fix: Переменные необходимо вынести из функции в начало файла

--- js/script.js
+++ js/script.js
@@ -3,15 +3,13 @@ let popup = root.querySelector('.popup');
 let formElement = popup.querySelector('.popup__form');
 let btnProfileChange = root.querySelector('.profile__btn-change');
 let btnPopupClose = popup.querySelector('.popup__btn-close');
+let nameInput = formElement.querySelector('.popup__input_func_name');
+let jobInput = formElement.querySelector('.popup__input_func_role');
+let name = root.querySelector('.profile__name');
+let job = root.querySelector('.profile__role');
 
 //открытие попапа
 function openForm() {
-  let nameInput = formElement.querySelector('.popup__input_func_name');
-  let jobInput = formElement.querySelector('.popup__input_func_role');
-
-  let name = root.querySelector('.profile__name');
-  let job = root.querySelector('.profile__role');
-
   nameInput.value = name.textContent.trim();
   jobInput.value = job.textContent.trim();
 
@@ -28,13 +26,6 @@ btnPopupClose.addEventListener('click', closeForm);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
-  // Находим поля формы в DOM
-  let nameInput = formElement.querySelector('.popup__input_func_name');
-  let jobInput = formElement.querySelector('.popup__input_func_role');
-  // имя и роль на странице
-  let name = root.querySelector('.profile__name');
-  let job = root.querySelector('.profile__role');
 
    name.textContent = nameInput.value;
    job.textContent = jobInput.value;
