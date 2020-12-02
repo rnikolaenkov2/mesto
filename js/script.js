a7c05eec2ce17facd29d8c422f8603c2430296fc
Roman Nikolaenkov <rnikolaenkov@gmail.com>
2020-12-02 11:47:23 +0300

    инициализация карточки, отображение через template

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
-let root = document.querySelector('.root');
-let popup = root.querySelector('.popup');
-let formElement = popup.querySelector('.popup__form');
-let btnProfileChange = root.querySelector('.profile__btn-change');
-let btnPopupClose = popup.querySelector('.popup__btn-close');
-let nameInput = formElement.querySelector('.popup__input_func_name');
-let jobInput = formElement.querySelector('.popup__input_func_role');
-let name = root.querySelector('.profile__name');
-let job = root.querySelector('.profile__role');
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const root = document.querySelector('.root');
+const popup = root.querySelector('.popup');
+const formElement = popup.querySelector('.popup__form');
+const btnProfileChange = root.querySelector('.profile__btn-change');
+const btnPopupClose = popup.querySelector('.popup__btn-close');
+const nameInput = formElement.querySelector('.popup__input_func_name');
+const jobInput = formElement.querySelector('.popup__input_func_role');
+const name = root.querySelector('.profile__name');
+const job = root.querySelector('.profile__role');
+const cardTemplate = root.querySelector('#card').content;
+const places = root.querySelector('.places');
+
+
+
+init(initialCards);
+
+//инициализация карточек
+function init(cards) {
+  cards.forEach((card) => {
+    let cardElement = cardTemplate.cloneNode(true);
+    console.log(cardElement.querySelector('.places__img'));
+    cardElement.querySelector('.places__img').setAttribute('src', card.link);
+    cardElement.querySelector('.places__title').textContent = card.name;
+    places.prepend(cardElement);
+  });
+}
 
 //открытие попапа
 function openForm() {
